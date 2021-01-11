7416127c17e6ab01e209476e7d2c2700704107f6
Matt Harasymczuk <matt@astrotech.io>
2021-01-11 15:04:30 +0100

    Sonar

--- bin/test-static.sh
+++ bin/test-static.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v $(pwd):/usr/src sonarsource/sonar-scanner-cli
+docker run --rm -e SONAR_HOST_URL="http://sonarqube:9000" -v $(pwd):/usr/src sonarsource/sonar-scanner-cli
